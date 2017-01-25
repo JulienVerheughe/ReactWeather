e3a29c79412ead8d5a4edb4bdc708534563e4dbf
JulienVerheughe <julienverheughe1992@gmail.com>
2017-01-25 10:20:01 +0100

    style weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -32,7 +32,7 @@ var Weather = React.createClass({
 
     function renderMessage (){
       if (isLoading === true){
-        return <h3>Loading...</h3>;
+        return <h3  className="text-center">Loading...</h3>;
       } else if (temp && location){
         return <WeatherMessage location={location} temp={temp}/>;
       }
@@ -40,7 +40,7 @@ var Weather = React.createClass({
 
     return(
       <div>
-        <h1>Get Weather</h1>
+        <h3 className="text-center">Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
